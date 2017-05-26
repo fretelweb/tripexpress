cc875f7270fb04066eeb1ce5a46a041b570b6d70
Ronny Fretel <ronny@fretelweb.com>
2017-05-26 18:37:25 -0500

    update Actualizado de 2.x a 3.1.4

--- src/application/language/makedonski/statistics_lang.php
+++
@@ -1,9 +0,0 @@
-<?php
-$lang['Statistics'] = "Статистика";
-$lang['Tickets sold'] = "Продадени билети";
-$lang['One way tickets income'] = "Промет од едносмерни вилети";
-$lang['Round trip tickets income'] = "Промет од повратни вилети";
-$lang['Total income'] = "Вкупно промет";
-$lang['Update'] = "Ажурирај";
-
-?>
